5c260ebe5bc7432ee7a8a06cfd74e7587d572f76
Hoodrn03 <Hoodrn03@gmail.com>
2018-11-29 16:23:21 +0000

    Created new Class
    I have created a new class which will be used to place, manage and maintain a single tree object in the game world. Next, I need to populate the class with functions.

---
+++ Game Code/Colony Sim/Colony Sim/inc/WoodResource.h
@@ -0,0 +1,49 @@
+/*! \file Grid.h
+*
+* This will be used to create and manage the deployment and maintainment of resources within the game world,
+* it will control a single tree on the map. 
+*
+*/
+
+#pragma once
+
+#include "defs.h"
+
+/*! \class This will be used to create a single tree in the game world. */
+class WoodResource
+{
+
+public:
+
+	//--------------------------------------------------------
+	/*! \fn Constructor
+	*
+	*/
+	WoodResource();
+
+public:
+
+	//--------------------------------------------------------
+	/*! \fn Deconstructor
+	*
+	*/
+	~WoodResource();
+
+	// Data Members 
+
+private:
+
+	// Member Functions 
+
+public:
+
+	// Todo Create Tree object.
+
+	// Todo Place Tree object.
+
+	// Todo Draw Tree object.
+
+	// Todo Update Tree object. 
+
+
+};
---
+++ Game Code/Colony Sim/Colony Sim/src/WoodResource.cpp
@@ -0,0 +1,23 @@
+/*! \file WoodResource.cpp
+*
+* This will hold all of the definitions for the class WoodResource.
+*
+*/
+
+#include "../inc/WoodResource.h"
+
+//--------------------------------------------------------
+/*! \fn Constructor
+*
+*/
+WoodResource::WoodResource()
+{
+}
+
+//--------------------------------------------------------
+/*! \fn Deconstructor
+*
+*/
+WoodResource::~WoodResource()
+{
+}
